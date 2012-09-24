59769aedf0cdbc1d2c8d07329395b699cdd7aa4d
Tim Ombasa <tim.ombasa@gmail.com>
2012-09-24 16:47:36 -0700

    Adding a shopping cart to the online store:
    Creating a Cart object and using a session object to locate the same cart in subsequent requests
    Adding method to make the Cart accessible to all our controllers
    Adding r/ships between carts and line items, and between line items and products
    Adding a button to post a product to a cart, thus creating a new line item.

---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3            #matching on id attributes
+    assert_select 'h3', 'Programming Ruby 1.9' #matching on element names
+    assert_select '.price', /\$[,\d]+\.\d\d/   #matching on class attributes
+  end
+
+end
