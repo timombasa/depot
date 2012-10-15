8dc37fb1a845c04f9507814f0f7b5f29df6e774d
Tim Ombasa <tim.ombasa@gmail.com>
2012-10-15 12:46:08 -0700

    Additional test fixtures for unique and duplicate products. Also, moving cart rendering to the sidebar instead of separate page.

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "add unique products" do
+    cart = Cart.create
+    coffeescript_Book = products(:coffeescript)
+    rails_Book = products(:railstest)
+
+    cart.add_product(rails_Book.id).save!
+    cart.add_product(coffeescript_Book.id).save!
+
+    assert_equal 2, cart.line_items.size
+    assert_equal coffeescript_Book.price + rails_Book.price, cart.total_price
+  end
+
+  test "add duplicate products" do
+    cart = Cart.create
+    ruby = products(:ruby)
+
+    cart.add_product(ruby.id).save!
+    cart.add_product(ruby.id).save!
+  end
+
 end
