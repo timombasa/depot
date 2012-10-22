cbed12dce43fb5f51480d6b6ab53452b94cde53b
Tim Ombasa <tim.ombasa@gmail.com>
2012-10-22 16:36:56 -0700

    Capturing an order: Checkout button to shopping cart that creates new order. Test to ensure item in cart b4 order. LineItems from cart added to the order, then validation, saving and redirect back to store url.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,9 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
+
   def total_price
     product.price * quantity
   end
