21e9e18b22c3270286438b1451a382a4c7fec6e4
Tim Ombasa <tim.ombasa@gmail.com>
2012-10-25 13:30:49 -0700

    Creating form to capture details for order. Adding validation for error display plus plugin for order list pagination.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,22 +1,18 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
   belongs_to :order
   belongs_to :product
   belongs_to :cart
+  attr_accessible :cart_id, :product_id
 
   def total_price
     product.price * quantity
-  end
-
-
-  def decrement_quantity(line_item_id)
-    current_item = LineItem.find_by_id(line_item_id)
-
-    if current_item.quantity > 1
-      current_item.quantity -= 1
-    else
-      current_item.destroy
-    end
-    current_item
   end
 end
